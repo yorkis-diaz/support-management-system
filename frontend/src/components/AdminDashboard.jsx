f781cb387020bd77e61dcbb45b51217a43ea2aab
Yorkis Diaz <yorkisdiaz@yorkiss-mbp.myfiosgateway.com>
2024-04-12 13:01:26 -0400

    fix: loading segment bug adjusted v2.4

--- frontend/src/components/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard.jsx
@@ -39,7 +39,7 @@ const AdminDashboardComponent = () => {
       if (!isLoading) {
         dispatch(fetchTicketsIfNeeded());
       }
-    }, [isLoading, dispatch]);
+    }, []);
 
     const handleRefresh = () => {
       dispatch(fetchTickets());
